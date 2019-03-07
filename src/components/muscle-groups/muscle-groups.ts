de9fe08d8c0be1f0b83f68bf50ce886d418d5be0
Anthony Thomas <antwonthegreat@yahoo.com>
2019-03-07 10:27:25 -0700

    Adding create modal to muscle groups

--- src/components/muscle-groups/muscle-groups.ts
+++ src/components/muscle-groups/muscle-groups.ts
@@ -3,12 +3,15 @@ import '@vaadin/vaadin-button';
 import '@vaadin/vaadin-dialog/vaadin-dialog';
 import '@vaadin/vaadin-text-field/vaadin-text-field';
 import './muscle-group-item';
+import '@material/mwc-fab/mwc-fab';
+import '@vaadin/vaadin-tabs/vaadin-tabs';
+import '@vaadin/vaadin-tabs/vaadin-tab';
 
 import {connectMixin} from '@leavittsoftware/titanium-elements/lib/titanium-redux-connect-mixin';
 import {customElement, property} from '@polymer/decorators';
 import {html, PolymerElement} from '@polymer/polymer';
 
-// import {createItemAsync, deleteItemAsync} from '../../actions/user-to-workout-type-actions';
+import {createItemAsync} from '../../actions/workout-type-actions';
 // import LiftType from '../../model/LiftType';
 import {ApplicationState} from '../../model/state/ApplicationState';
 import WorkoutType from '../../model/WorkoutType';
@@ -18,8 +21,9 @@ import {store} from '../../store';
 @customElement('muscle-groups') export class MuscleGroups extends connectMixin
 (store, PolymerElement) {
   @property() workoutTypes: Array<WorkoutType>;
-  @property() addWorkoutTypeOpened: boolean;
+  @property() addModalOpened: boolean;
   @property() newWorkoutTypeName: string;
+  @property() selectedTabIndex: number;
 
   connectedCallback() {
     super.connectedCallback();
@@ -36,22 +40,26 @@ import {store} from '../../store';
     console.log(this.workoutTypes);
   }
 
-  protected _openAddWorkoutTypeDialog() {
-    this.addWorkoutTypeOpened = true;
+  protected _openAddModal() {
+    this.addModalOpened = true;
   }
 
-  protected _closeAddWorkoutTypeDialog() {
-    this.addWorkoutTypeOpened = false;
+  protected _closeAddModal() {
+    this.addModalOpened = false;
   }
 
-  protected _saveWorkoutTypeDisabled(): boolean {
-    return true;
+  protected _saveWorkoutTypeDisabled(newWorkoutTypeName: string): boolean {
+    return !newWorkoutTypeName;
   }
 
-  // _createWorkoutType() {
-  //   store.dispatch<any>(createItemAsync({Name: this.newWorkoutName}));
-  //   this.blankDialogOpened = false;
-  // }
+  _createWorkoutType() {
+    store.dispatch<any>(createItemAsync({Name: this.newWorkoutTypeName}));
+    this.addModalOpened = false;
+  }
+
+  protected _areEqual(a: any, b: any): boolean {
+    return a === b;
+  }
 
   static get template() {
     return html`<style>
@@ -79,6 +87,17 @@ import {store} from '../../store';
     height: 24px;
   }
 
+  mwc-fab {
+    position: fixed;
+    bottom: 16px;
+    right: 16px;
+  }
+
+  add-lift-type,
+  add-muscle-group {
+    @apply --layout-vertical;
+  }
+
   [hidden] {
     display: none;
   }
@@ -98,18 +117,32 @@ import {store} from '../../store';
     <muscle-group-item workout-type="[[item]]" on-click="_handleMuscleGroupClick">[[item.Name]]</muscle-group-item>
   </template>
 </main-content>
-<vaadin-dialog opened="{{addWorkoutTypeOpened}}">
+<mwc-fab on-click="_openAddModal" icon="add"></mwc-fab>
+<vaadin-dialog opened="{{addModalOpened}}">
   <template>
     <style>
       :host {
         @apply --layout-vertical;
       }
     </style>
-    <vaadin-text-field value="{{newWorkoutTypeName}}" label="Name"></vaadin-text-field>
-    <action-buttons>
-      <vaadin-button cancel on-click="_closeAddWorkoutTypeDialog">Cancel</vaadin-button>
-      <vaadin-button disabled="[[_saveWorkoutTypeDisabled]]" on-click="_createWorkoutType">Save</vaadin-button>
-    </action-buttons>
+    <vaadin-tabs selected="{{selectedTabIndex}}">
+        <vaadin-tab>Muscle Group</vaadin-tab>
+        <vaadin-tab>Lift</vaadin-tab>
+    </vaadin-tabs>
+    <add-muscle-group hidden$="[[!_areEqual(selectedTabIndex,0)]]">
+      <vaadin-text-field value="{{newWorkoutTypeName}}" label="Name"></vaadin-text-field>
+      <action-buttons>
+        <vaadin-button cancel on-click="_closeAddModal">Cancel</vaadin-button>
+        <vaadin-button disabled="[[_saveWorkoutTypeDisabled(newWorkoutTypeName)]]" on-click="_createWorkoutType">Save</vaadin-button>
+      </action-buttons>
+    </add-muscle-group>
+    <add-lift-type hidden$="[[!_areEqual(selectedTabIndex,1)]]">
+      <vaadin-text-field value="{{newLiftTypeName}}" label="Name"></vaadin-text-field>
+      <action-buttons>
+        <vaadin-button cancel on-click="_closeAddModal">Cancel</vaadin-button>
+        <vaadin-button disabled="[[_saveLiftTypeDisabled(newLiftTypeName)]]" on-click="_createLiftType">Save</vaadin-button>
+      </action-buttons>
+    </add-lift-type>
   </template>
 </vaadin-dialog>
 `;
